c1ea934fa90b79dfd16f7242a0e2d6759ee64141
Adeney <philocity@proton.me>
2026-09-09 15:23:04 +0100

    sequence trim

---
+++ scripts/Sequence_trimming.py
@@ -0,0 +1,5 @@
+# Sequence trimming
+"""The consensus sequence has been validated to contain the Lipoprotein signal peptide (Sec/SPII) from aa 1 to aa 21, a trim must be initiated to derive the mature protein"""
+
+Conseq = "MQLNKVLKGLMIALPVMAIAACSSNKNASNDQSGEGMLGAGTGMDANGNGGNMSSEEQARLQMQQLQQNNIVYFDLDKYDIRSDFAAMLDAHANFLRSNPSYKVTVEGHADERGTPEYNIALGERRANAVKMYLQGKGVSADQISIVSYGKEKPAVLGHDEAAYAKNRRAVLVY"
+print (Conseq [21:])
